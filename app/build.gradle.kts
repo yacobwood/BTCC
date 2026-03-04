f7e4b4509d6af85dd524275584b3e9c99a50f253
jakewood <jake.wood@altovita.com>
2026-03-04 10:46:19 +0000

    chore: bump application version to 1.4 (versionCode 5).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.btccfanhub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
